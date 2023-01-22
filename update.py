97932d961f13b4d6d40dbc896e8ac32c6ab1ab9b
Benji <97198967+BenPI88@users.noreply.github.com>
2023-01-22 16:13:28 -0600

    Update update.py

--- update.py
+++ update.py
@@ -1,4 +1,4 @@
 import os
 print("Updating...")
-os.system("cd .. && rm -rf plugin-unpacker && git clone https://https://github.com/BenPI88/plugin-unpacker && cd plugin-unpacker")
+os.system("cd .. && rm -rf plugin-unpacker && git clone https://github.com/BenPI88/plugin-unpacker && cd plugin-unpacker")
 print("Updated.")
